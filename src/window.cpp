da58cf88922aee1cdd25dbaf1b19d2522edd05f9
Lewand118 <w.lewandowski118@wp.pl>
2019-07-13 23:43:33 +0200

    blad
    w klasie window chyba

--- src/window.cpp
+++ src/window.cpp
@@ -1,2 +1,23 @@
-#include "c_Window"
+/*#include "window.hpp"
 
+c_window::c_window()
+	:window(sf::VideoMode(Width, Height), "test_window", sf::Style::Default)
+{
+}
+
+void c_window::run()
+{
+
+	while (window.isOpen())
+	{
+		window.clear(sf::Color::Black);
+		sf::Event e;
+		while (window.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+				window.close();
+		}
+
+		window.display();
+	}
+}*/
--- src/window.hpp
+++ src/window.hpp
@@ -1,9 +1,21 @@
-#include <SFML\Graphics.hpp>
+/*#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
 #include <iostream>
 
-class c_Window
+class c_window :public sf::Drawable
 {
+	sf::RenderWindow window;
+	sf::CircleShape shape; // just for test, to delete later
 
+	void draw(sf::RenderTarget& target, sf::RenderStates state) const override;
 
+public:
+	c_window();
+	~c_window() = default;
 
-};
+	const int Width = 1600;
+	const int Height = 1200;
+
+	void run();
+
+};*/
