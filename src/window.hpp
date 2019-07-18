68b299242ef25a6982cfb00c1bd7475d118a4130
Lewand118 <w.lewandowski118@wp.pl>
2019-07-18 20:21:51 +0200

    draw?

--- src/window.hpp
+++ src/window.hpp
@@ -7,7 +7,8 @@ class c_window
 	sf::RenderWindow window;
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 	c_Map* map;
-
+	
+	//sf::RectangleShape shape;
 public:
 	c_window(sf::Vector2f windowSize, c_Map* map);
 
